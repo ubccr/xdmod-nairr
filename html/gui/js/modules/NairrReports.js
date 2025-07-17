7c3e1d7ea9030097e4bda1579121398d00c8e90c
Rose Tovar <15618284+rvtovar@users.noreply.github.com>
2025-07-17 15:19:58 -0400

    Nairr Work from Previous Repo to Nairr Custom Report Tab Code (#1)
    These include updates so that the neweste versions of cloudbank work with Nairr, along with creation of the Nairr Report Tab
    
    @authored by Alex Tovar July 17th 2025

---
+++ html/gui/js/modules/NairrReports.js
@@ -0,0 +1,85 @@
+/**
+ * NAIRR Reports Module for XDMoD Portal
+ * @author Alex Tovar
+ * @date 2025-07-14
+
+ *
+ * This module displays NAIRR reports fetched from the `/custom_reports/reports` endpoint.
+ * - It creates a main panel area containing thumbnails and metadata for each report.
+ * - Each report displays its title, version, description, and a download link.
+ * - The module is integrated into XDMoD's main tab panel and can be activated programmatically.
+ *
+ * Usage and Setup:
+ * - The module ID is "nairr_reports". To display this tab, activate it via the main_tab_panel.
+ * - The reports list is loaded from the `/custom_reports/reports` endpoint using a JsonStore.
+ * - Each report's thumbnail and download link are constructed based on the report's name.
+ * - Toolbar is currently disabled.
+ *
+ * Public API:
+ * - setConfig(config, name): Activates the "nairr_reports" tab in the main_tab_panel.
+ * - initComponent(): Initializes the UI and data fetching logic for the module.
+ *
+ */
+// ===========================================================================
+XDMoD.Module.NairrReports = function (config) {
+  XDMoD.Module.NairrReports.superclass.constructor.call(this, config);
+};
+
+Ext.apply(XDMoD.Module.NairrReports, {
+  setConfig: function (config, name) {
+    var tabPanel = Ext.getCmp("main_tab_panel");
+
+    tabPanel.setActiveTab("nairr_reports");
+  }, // setConfig()
+}); //
+
+Ext.extend(XDMoD.Module.NairrReports, XDMoD.PortalModule, {
+  module_id: "nairr_reports", // <-- rename this (see the section on REPORT CHECKBOX for how to name this)
+
+  charStore: false,
+  usesToolbar: false,
+
+  initComponent: function () {
+    // ------------------------------------------------------------------
+
+    var mainArea = new Ext.Panel({
+      title: "NAIRR Reports",
+      region: "center",
+      items: [
+        new Ext.DataView({
+          store: new Ext.data.JsonStore({
+            autoDestroy: true,
+            autoLoad: true,
+            url: XDMoD.REST.prependPathBase("/custom_reports/reports"),
+            storeId: "customReportStore",
+            root: "report_list",
+            idProperty: "name",
+            fields: ["name", "version", "title", "description"],
+          }),
+          tpl: new Ext.XTemplate(
+            '<tpl for=".">',
+            '<div class="custom-report-thumb-wrap" id="{name}">',
+            '  <div class="custom-report-thumb"><img src="' +
+              XDMoD.REST.prependPathBase("/custom_reports/thumbnail/") +
+              '{name}" title="{name}"></div>',
+            '  <div class="custom-report-thumb-desc"><h2 class="custom-report-thumb-title">{title}</h2><p>Version: {version}</p><p>{description}</p>',
+            '   <div style="text-align: center;"><p><a href="' +
+              XDMoD.REST.prependPathBase("/custom_reports/report/") +
+              '{name}" name="{name}">Download</a></div>',
+            "  </div>",
+            "</div>",
+            "</tpl>",
+            '<div class="x-clear"></div>',
+          ),
+          emptyText: "No reports available.",
+        }),
+      ],
+    }); //mainArea
+
+    Ext.apply(this, {
+      items: [mainArea],
+    });
+
+    XDMoD.Module.NairrReports.superclass.initComponent.apply(this, arguments);
+  }, //initComponent
+}); //XDMoD.Module.NewModule
--- scripts/generate_resources.py
+++ scripts/generate_resources.py
@@ -1,19 +1,26 @@
-
 sql = """
-SELECT RES.RESOURCE_ID,
-	RES.RESOURCE_NAME,
-        RES.DESCRIPTION,
-	RTYPE.RESOURCE_TYPE,
-	RES.PRODUCTION_BEGIN_DATE,
-	RES.PRODUCTION_END_DATE
-FROM "xras"."resources" AS RES
-JOIN "xras"."allocations_process_resources" AS APRES ON RES.RESOURCE_ID = APRES.RESOURCE_ID
-JOIN "xras"."allocations_processes" AS AP ON AP.ALLOCATIONS_PROCESS_ID = APRES.ALLOCATIONS_PROCESS_ID
-LEFT JOIN "xras"."resource_types" AS RTYPE ON RTYPE.RESOURCE_TYPE_ID = RES.RESOURCE_TYPE_ID
-WHERE AP.ALLOCATIONS_PROCESS_NAME = 'National Artificial Intelligence Research Resource'
-	AND RES.PRODUCTION_BEGIN_DATE IS NOT NULL
-ORDER BY RES.PRODUCTION_BEGIN_DATE ASC,
-	RES.RESOURCE_NAME ASC
+SELECT
+  RES.RESOURCE_ID,
+  RES.RESOURCE_NAME,
+  RES.DESCRIPTION,
+  RTYPE.RESOURCE_TYPE,
+  RES.PRODUCTION_BEGIN_DATE,
+  RES.PRODUCTION_END_DATE,
+  TRIM(
+    COALESCE(O.organization_abbr, O.organization_name)
+  ) AS "organization"
+FROM
+  "xras"."resources" AS RES
+  JOIN "xras"."allocations_process_resources" AS APRES ON RES.RESOURCE_ID = APRES.RESOURCE_ID
+  JOIN "xras"."allocations_processes" AS AP ON AP.ALLOCATIONS_PROCESS_ID = APRES.ALLOCATIONS_PROCESS_ID
+  JOIN "xras"."organizations" as O ON O.organization_id = RES.organization_id
+  LEFT JOIN "xras"."resource_types" AS RTYPE ON RTYPE.RESOURCE_TYPE_ID = RES.RESOURCE_TYPE_ID
+WHERE
+  AP.ALLOCATIONS_PROCESS_NAME = 'National Artificial Intelligence Research Resource'
+  AND RES.PRODUCTION_BEGIN_DATE IS NOT NULL
+ORDER BY
+  RES.PRODUCTION_BEGIN_DATE ASC,
+  RES.RESOURCE_NAME ASC
 """
 
 import psycopg2
@@ -22,46 +29,46 @@ import re
 import configparser
 
 
-rtype_map = {
-        'Compute': 'HPC',
-        'Cloud': 'Cloud',
-        'Program': 'Program'
-}
+rtype_map = {"Compute": "HPC", "Cloud": "Cloud", "Program": "Program"}
+
 
 def demangle_name(inname):
 
     name = inname
-    r = re.compile(r'^([^(]+)')
+    r = re.compile(r"^([^(]+)")
     mtch = r.match(name)
     if mtch:
         name = mtch.group(0)
 
-    return re.sub(r'[^\w]', '-', name)
+    return re.sub(r"[^\w]", "-", name)
+
 
 def main():
 
-    XDMOD_CONFIG_PATH='/etc/xdmod'
+    XDMOD_CONFIG_PATH = "/data/www/xdmod/etc"
 
     config = configparser.ConfigParser()
-    config.read(f'{XDMOD_CONFIG_PATH}/portal_settings.ini')
+    config.read(f"{XDMOD_CONFIG_PATH}/portal_settings.ini")
 
     resources = {}
 
-    with open(f'{XDMOD_CONFIG_PATH}/resources.json', 'r') as resources_file:
+    with open(f"{XDMOD_CONFIG_PATH}/resources.json", "r") as resources_file:
         for res in json.load(resources_file):
-            resources[res['resource']] = {'fact': res}
+            resources[res["resource"]] = {"fact": res}
 
-    with open(f'{XDMOD_CONFIG_PATH}/resource_specs.json', 'r') as resourcespecs_file:
+    with open(f"{XDMOD_CONFIG_PATH}/resource_specs.json", "r") as resourcespecs_file:
         for res in json.load(resourcespecs_file):
-            resources[res['resource']]['specs'] = res
+            resources[res["resource"]]["specs"] = res
 
     print(json.dumps(resources))
 
-    with psycopg2.connect(database=config['tgcdbmirror']['database'].strip("'"),
-                        host=config['tgcdbmirror']['host'].strip("'"),
-                        user=config['tgcdbmirror']['user'].strip("'"),
-                        password=config['tgcdbmirror']['pass'].strip("'"),
-                        port=config['tgcdbmirror']['port'].strip("'")) as conn:
+    with psycopg2.connect(
+        database=config["tgcdbmirror"]["database"].strip("'"),
+        host=config["tgcdbmirror"]["host"].strip("'"),
+        user=config["tgcdbmirror"]["user"].strip("'"),
+        password=config["tgcdbmirror"]["pass"].strip("'"),
+        port=config["tgcdbmirror"]["port"].strip("'"),
+    ) as conn:
 
         with conn.cursor() as curs:
             curs.execute(sql)
@@ -71,41 +78,45 @@ def main():
 
                 if resource not in resources:
                     resources[resource] = {
-                        'fact': {
-                            'resource': resource,
-                            'resource_type': rtype_map[data[3]],
-                            'name': data[1],
-                            'description': data[2],
-                            'resource_allocation_type': 'CPUNode',
-                            'timezone': 'EST',
-                            'pi_column': 'account_name'
+                        "fact": {
+                            "resource": resource,
+                            "resource_type": rtype_map[data[3]],
+                            "name": data[1],
+                            "description": data[2] or "",
+                            "resource_allocation_type": "CPUNode",
+                            "timezone": "EST",
+                            "pi_column": "account_name",
+                            "organization": data[6],
                         },
-                        'specs': {
-                            'resource': resource,
-                            'start_date': data[4].strftime('%Y-%m-%d'),
-                            'cpu_node_count': 1,
-                            'cpu_processor_count': 1,
-                            'cpu_ppn': 1,
-                            'gpu_node_count': 0,
-                            'gpu_processor_count': 0,
-                            'gpu_ppn': 0
-                        }
+                        "specs": {
+                            "resource": resource,
+                            "start_date": data[4].strftime("%Y-%m-%d"),
+                            "cpu_node_count": 1,
+                            "cpu_processor_count": 1,
+                            "cpu_ppn": 1,
+                            "gpu_node_count": 0,
+                            "gpu_processor_count": 0,
+                            "gpu_ppn": 0,
+                        },
                     }
                     if data[5]:
-                        resources[resource]['specs']['end_date'] = data[5].strftime('%Y-%m-%d')
+                        resources[resource]["specs"]["end_date"] = data[5].strftime(
+                            "%Y-%m-%d"
+                        )
 
         rfact = []
         rspec = []
 
         for info in resources.values():
-            rfact.append(info['fact'])
-            rspec.append(info['specs'])
+            rfact.append(info["fact"])
+            rspec.append(info["specs"])
 
-        with open('resources.json', 'w') as filep:
+        with open(f"{XDMOD_CONFIG_PATH}/resources.json", "w") as filep:
             json.dump(rfact, filep, indent=4)
 
-        with open('resource_specs.json', 'w') as filep:
+        with open(f"{XDMOD_CONFIG_PATH}/resource_specs.json", "w") as filep:
             json.dump(rspec, filep, indent=4)
+
 
 if __name__ == "__main__":
     main()
